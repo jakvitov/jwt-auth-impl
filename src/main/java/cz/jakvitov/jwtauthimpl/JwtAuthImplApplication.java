563ea6e400f0a64e35995852e22d1ce6edb11808
Jakub Vítovec <jakvitov@gmail.com>
2023-04-26 17:52:51 +0200

    Initial commit

---
+++ src/main/java/cz/jakvitov/jwtauthimpl/JwtAuthImplApplication.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.jwtauthimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/jakvitov/jwtauthimpl/JwtAuthImplApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.jwtauthimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
